67eb27a87c5d6e1f0249c12cb4bd7f1efc29f5c6
camdelay16 <camdelay16@gmail.com>
2024-10-15 15:18:35 -0400

    updated product model

---
+++ models/product.js
@@ -0,0 +1,20 @@
+const { Schema } = require('mongoose')
+const brandSchema = require('./brand')
+const categorySchema = require('./category')
+
+const Product = new Schema (
+    {
+        productName: { type: String, required: true },
+        supplyType: { type: Schema.Types.ObjectId, ref: "category_id" },
+        brandId:  { type: Schema.Types.ObjectId, ref: "brand_id" },
+        price:  { type: Number, required: true },
+        isAvailable:  { type: Boolean, required: true },
+        stock:  { type: Number, required: true },
+        imageURL: { type: String, required: true },
+        isEcoFriendly:  { type: Boolean, required: true },
+        isWaterSoluble:  { type: Boolean, required: true },
+        isChildSafe:  { type: Boolean, required: true },
+        description: { type: String, required: true },
+    },
+    { timestamps: true }
+)
