669080a90372cab20bf25264c61e4057f47f1c77
camdelay16 <camdelay16@gmail.com>
2024-10-16 18:03:58 -0400

    updated server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('./db')
+const path = require('path')
 const brandController = require('./controllers/brandController.js')
 const categoryController = require('./controllers/categoryController.js')
 const productController = require('./controllers/productController.js')
@@ -15,13 +16,14 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.listen(PORT, () => {
     console.log(`express server running on ${PORT}`)
 })
 
 app.get ('/', (req,res) => {
-    res.send(`Taubman's Art Supplies`)
+    res.sendFile(path.join(__dirname,'client','index.html'))
 })
 
 //Brand Routes
